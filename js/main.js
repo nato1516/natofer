9cb297c7a608f8a9021ab38955e4e416662fdabc
nato1516 <163149975+nato1516@users.noreply.github.com>
2025-11-19 12:15:25 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -108,3 +108,19 @@ function type() {
 }
 
 type();
+gsap.registerPlugin(ScrollTrigger);
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card, i) => {
+    gsap.to(card, {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: card,
+            start: "top center+=100",
+            toggleActions: "play none none reverse"
+        }
+    });
+});
